58d14499d9e45b2dd14dfd7c013a5f2233f6316c
nayem1816 <2191081070@uttarauniversity.edu.bd>
2021-04-18 09:23:51 +0600

    some change design

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -2,6 +2,10 @@ import { faMapMarker } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import facebook from "../../../images/icon/facebook.png";
+import google from "../../../images/icon/google.png";
+import whatsapp from "../../../images/icon/whatsapp.png";
+import youtube from "../../../images/icon/youtube.png";
 
 const Footer = () => {
   return (
@@ -10,8 +14,8 @@ const Footer = () => {
         <div className="row container">
           <div className="col-md-3 text-center text-md-start">
             <Link
-              style={{ fontSize: "30px" }}
-              className="navbar-brand text-dark rock-salt"
+              style={{ fontSize: "30px", color:'red' }}
+              className="navbar-brand rock-salt"
               to="/"
             >
               Fiver Network
@@ -21,9 +25,7 @@ const Footer = () => {
                 <FontAwesomeIcon icon={faMapMarker} />
               </span>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut culpa
-              delectus deleniti! Eos, ab. Culpa cumque officia deserunt quas,
-              iste beatae inventore corporis temporibus hic, eveniet, velit amet
-              sed libero!
+              delectus deleniti! Eos, ab. Culpa cumque officia deserunt quas.
             </p>
           </div>
           <div className="col-md-3 text-center text-md-start">
@@ -84,12 +86,41 @@ const Footer = () => {
           </div>
           <div className="col-md-3 text-center text-md-start">
             <h2>News letter</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut culpa
-              delectus deleniti! Eos, ab. Culpa cumque officia deserunt quas,
-              iste beatae inventore corporis temporibus hic, eveniet, velit amet
-              sed libero!
-            </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <div className="">
+              <Link to="">
+                <img
+                  style={{ height: "40px", width: "40px" }}
+                  className="m-2"
+                  src={facebook}
+                  alt=""
+                />
+              </Link>
+              <Link to="">
+                <img
+                  style={{ height: "40px", width: "40px" }}
+                  className="m-2"
+                  src={google}
+                  alt=""
+                />
+              </Link>
+              <Link to="">
+                <img
+                  style={{ height: "40px", width: "40px" }}
+                  className="m-2"
+                  src={whatsapp}
+                  alt=""
+                />
+              </Link>
+              <Link to="">
+                <img
+                  style={{ height: "40px", width: "40px" }}
+                  className="m-2"
+                  src={youtube}
+                  alt=""
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
